39c5af87dfe98e92ae23aafbe1b054e9e5dd449f
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2020-04-28 10:38:08 +0200

    item

---
+++ Raunstrup.Contract/Services/IItemService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Raunstrup.Contract.DTOs;
+
+namespace Raunstrup.Contract.Services
+{
+    public interface IItemService
+    {
+        Task<IEnumerable<ItemDto>> GetItemsAsync();
+
+        Task<ItemDto> GetItemAsync(int id);
+
+        Task AddAsync(ItemDto item);
+
+        Task UpdateAsync(int id, ItemDto item);
+
+        Task RemoveAsync(int id);
+    }
+}
--- Raunstrup.UI/Models/ItemMapper.cs
+++ Raunstrup.UI/Models/ItemMapper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Raunstrup.Contract.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,21 @@ namespace Raunstrup.UI.Models
 {
     public class ItemMapper
     {
+        public static ItemViewModel Map(ItemDto dto)
+        {
+            return new ItemViewModel
+            { Id = dto.Id, Name = dto.Name, Price = dto.Price, Active = dto.Active };
+        }
 
+        public static IEnumerable<ItemViewModel> Map(IEnumerable<ItemDto> dtos)
+        {
+            return dtos.Select(x => Map(x)).AsEnumerable();
+        }
+
+        public static ItemDto Map(ItemViewModel view)
+        {
+            return new ItemDto
+            { Id = view.Id, Name = view.Name, Price = view.Price, Active = view.Active };
+        }
     }
 }
