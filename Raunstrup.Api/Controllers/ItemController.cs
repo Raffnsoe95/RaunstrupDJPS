650c04bc1d3dc03c44888211ecb80028ea315a16
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2020-04-29 08:46:45 +0200

    ItemApi

---
+++ Raunstrup.Api/Controllers/ItemController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Raunstrup.Api.Models;
+using Raunstrup.Contract.DTOs;
+using Raunstrup.BusinessLogic.ServiceInterfaces;
+
+namespace Raunstrup.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Movies")]
+    //[ApiVersion("1.0")]
+    //[ApiVersion("1.1")]
+    [ApiController]
+
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+
+        public ItemController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        // GET: api/Movies
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return _itemService.GetAll().Select(a => ItemMapper.Map(a));
+        }
+
+        // GET: api/Movies/5
+        [HttpGet("{id}", Name = "Get")]
+        //[ApiVersion("1.1")]
+        public ItemDto Get(int id)
+        {
+            return ItemMapper.Map(_itemService.Get(id));
+        }
+
+        // POST: api/Movies
+        [HttpPost]
+        public void Post([FromBody] ItemDto value)
+        {
+            _itemService.Create(ItemMapper.Map(value));
+        }
+
+        // PUT: api/Movies/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ItemDto value)
+        {
+            _itemService.Update(ItemMapper.Map(value));
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _itemService.Delete(id);
+        }
+    }
+}
--- Raunstrup.Api/Models/ItemMapper.cs
+++ Raunstrup.Api/Models/ItemMapper.cs
@@ -1,4 +1,5 @@
 ﻿//using Raunstrup.Contract.DTOs;
+using Raunstrup.Contract.DTOs;
 using Raunstrup.DataAccess.Model;
 using System;
 using System.Collections.Generic;
@@ -9,21 +10,21 @@ namespace Raunstrup.Api.Models
 {
     public class ItemMapper
     {
-        //public static Item Map(ItemDto dto)
-        //{
-        //    return new Item
-        //    { Id = dto.Id, Name = dto.Name, Price = dto.Price, Active = dto.Active };
-        //}
+        public static Item Map(ItemDto dto)
+        {
+            return new Item
+            { Id = dto.Id, Name = dto.Name, Price = dto.Price, Active = dto.Active };
+        }
 
-        //public static IEnumerable<ItemDto> Map(IEnumerable<Item> model)
-        //{
-        //    return model.Select(x => Map(x)).AsEnumerable();
-        //}
+        public static IEnumerable<ItemDto> Map(IEnumerable<Item> model)
+        {
+            return model.Select(x => Map(x)).AsEnumerable();
+        }
 
-        //public static ItemDto Map(Item model)
-        //{
-        //    return new ItemDto
-        //    { Id = model.Id, Name = model.Name, Price = model.Price, Active = model.Active };
-        //}
+        public static ItemDto Map(Item model)
+        {
+            return new ItemDto
+            { Id = model.Id, Name = model.Name, Price = model.Price, Active = model.Active };
+        }
     }
 }
