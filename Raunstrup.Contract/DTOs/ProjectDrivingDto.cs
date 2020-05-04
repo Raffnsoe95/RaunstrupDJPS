8726ead8825f3c08efb3a692d9fa8a915a2d0998
JCMcGraw <49905256+JCMcGraw@users.noreply.github.com>
2020-05-04 08:59:13 +0200

    ProjectDriving Added

---
+++ Raunstrup.Contract/DTOs/ProjectDrivingDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raunstrup.Contract.DTOs
+{
+    public class ProjectDrivingDto
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public int EmployeeId { get; set; }
+        public EmployeeDto Employee { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
