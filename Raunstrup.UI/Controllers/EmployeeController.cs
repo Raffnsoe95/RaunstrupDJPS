84246c5bb4cd4f3dacfcbbb8c57402a1499c02d3
Peder Riis <pede0821@koldinghfogvuc.dk>
2020-05-14 14:51:23 +0200

    rettet en fejl når man tilknyttede en medarbejder til projekt

--- Raunstrup.UI/Controllers/EmployeeController.cs
+++ Raunstrup.UI/Controllers/EmployeeController.cs
@@ -170,11 +170,19 @@ namespace Raunstrup.UI.Controllers
         }
         // GET: Employee
         public async Task<IActionResult> AddProjectEmployee(int id, string searchString)
-        { 
-            var employeeDtos = await _employeeService.GetEmployeesAsync().ConfigureAwait(false);
-            IEnumerable<EmployeeDto> filterdEmployeeDtos = await _employeeService.GetFilteredEmployeesAsync(searchString);
+        {
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                IEnumerable<EmployeeDto> filterdEmployeeDtos = await _employeeService.GetFilteredEmployeesAsync(searchString);
+                return View(EmployeeMapper.MapEst(filterdEmployeeDtos).ToList());
+            }
+            else {
+                var employeeDtos = await _employeeService.GetEmployeesAsync().ConfigureAwait(false);
 
-            return View(EmployeeMapper.MapEst(filterdEmployeeDtos).ToList());
+
+                return View(EmployeeMapper.MapEst(employeeDtos).ToList());
+            }
+          
         }
         //public async Task<IActionResult> AddProjectEmployeeToProject(int id, int projectid)
         //{
