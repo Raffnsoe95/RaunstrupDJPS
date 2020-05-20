04bb122e24d74158220dd943343fbc1f9e3bd384
JCMcGraw <49905256+JCMcGraw@users.noreply.github.com>
2020-05-20 12:55:31 +0200

    Opdatering og lokalisering af create projectdriving

--- Raunstrup.UI/Models/ProjectDrivingViewModel.cs
+++ Raunstrup.UI/Models/ProjectDrivingViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace Raunstrup.UI.Models
     public class ProjectDrivingViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Indtast venligst et antal enheder!")]
+        [Range(0.01, 100000, ErrorMessage = "Angiv venligst et antal enheder!")]
         public int Amount { get; set; }
         public int EmployeeId { get; set; }
         public EmployeeViewModel Employee { get; set; }
+        [Range(0.01, 100000, ErrorMessage = "Angiv venligst en korrekt enhedspris!")]
+        [Required(ErrorMessage = "Indtast venligst en enhedspris!")]
+        [DataType(DataType.Currency)]
         public decimal UnitPrice { get; set; }
         public int ProjectId { get; set; }
     }
